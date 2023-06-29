0ab137b1cd16e591e896fc5584acf4d02bf9f180
kvmhyvmvn <97383799+kvmhyvmvn@users.noreply.github.com>
2023-06-29 19:16:01 +0900

    23.06.29 Android

--- WorkSpace/LastCloneAppProject/app/src/main/java/com/example/lastcloneappproject/MainActivity.java
+++ WorkSpace/LastCloneAppProject/app/src/main/java/com/example/lastcloneappproject/MainActivity.java
@@ -5,14 +5,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.lastcloneappproject.databinding.ActivityMainBinding;
+import com.example.lastcloneappproject.databinding.ItemBannerBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
+
+        binding.sliderbanner.setAdapter(new SliderAdapter(banArr));
     }
+
+   int[] banArr = {R.drawable.img_banner1, R.drawable.img_banner2, R.drawable.img_banner3};
 }
